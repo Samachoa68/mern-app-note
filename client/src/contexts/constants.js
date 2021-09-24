aa6c5b6a600c1bb6c88c80a1763eeb6e4d854cb9
Samachoa Cool <lamgiatechnology@gmail.com>
2021-09-24 22:23:27 +0700

    up client to github

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,6 +1,6 @@
 export const apiUrl =
   process.env.NODE_ENV !== "production"
-    ? "http://localhost:5000/api"
+    ? "https://mern-app-note.herokuapp.com/api"
     : "somedeployedUrl";
 
 export const LOCAL_STORAGE_TOKEN_NAME = "learn_app";
